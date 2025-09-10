a8acc2775f1c07b02548f5f381bc64b5f6205f89
1xcodex <yishak.ab1216@gmail.com>
2025-09-10 03:40:22 -0700

    Complete change of repo

---
+++ movie-clone-app/src/Pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../../../../Movie-Clone-2025/netflix-clone/src/components/Header/Header";
+import Footer from "../../../../../Movie-Clone-2025/netflix-clone/src/components/Footer/Footer";
+import Banner from "../../../../../Movie-Clone-2025/netflix-clone/src/components/Banner/Banner";
+import RowList from "../../../../../Movie-Clone-2025/netflix-clone/src/components/Rows/RowList/RowList";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
